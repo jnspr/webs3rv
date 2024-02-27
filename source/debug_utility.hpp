c619dde6590ed84889d738524ce852b0def3b414
jsprenge <jnspr@outlook.de>
2024-02-27 14:45:01 +0100

    debug_utility: Move printConfig to debug_utility

---
+++ source/debug_utility.hpp
@@ -0,0 +1,9 @@
+#ifndef DEBUG_UTILITY_hpp
+#define DEBUG_UTILITY_hpp
+
+#include "config.hpp"
+
+/* Prints the given configuration to standard output */
+void printConfig(const ApplicationConfig &config);
+
+#endif // DEBUG_UTILITY_hpp
