bd68ba16ad6b74be1e07f6abe09cd091ca803d43
jsprenge <jsprenge@student.42wolfsburg.de>
2024-02-28 18:20:27 +0100

    timeout: Implement wrapper class for timeouts

---
+++ source/timeout.cpp
@@ -0,0 +1,45 @@
+#include "timeout.hpp"
+
+#include <ctime>
+#include <stdexcept>
+
+/* Constructs a timeout starting from the current time */
+Timeout::Timeout()
+{
+    reset();
+}
+
+/* Resets the timeout's starting time to the current time */
+void Timeout::reset()
+{
+    _startTime = getCurrentTime();
+    _isStopped = false;
+}
+
+/* Gets whether the timeout has expired or not */
+bool Timeout::isExpired()
+{
+    if (_isStopped)
+        return false;
+    uint64_t currentTime = getCurrentTime();
+    if (currentTime < _startTime)
+        throw std::runtime_error("Time moved backwards; did the system time change?");
+    return currentTime - _startTime >= DEFAULT_TIMEOUT_MS;
+}
+
+/* Stops the timeout so it can never expire */
+void Timeout::stop()
+{
+    _isStopped = true;
+}
+
+/* Queries the current time from the operating system */
+uint64_t Timeout::getCurrentTime()
+{
+    timespec ts;
+
+    if (clock_gettime(CLOCK_MONOTONIC_RAW, &ts) != 0)
+        throw std::runtime_error("Unable to query monotonic system time");
+
+    return ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
+}
---
+++ source/timeout.hpp
@@ -0,0 +1,37 @@
+#ifndef TIMEOUT_hpp
+#define TIMEOUT_hpp
+
+#include <stdint.h>
+
+/* Default timeout duration */
+#define DEFAULT_TIMEOUT_MS 5000
+
+class Timeout
+{
+public:
+    /* Constructs a timeout starting from the current time */
+    Timeout();
+
+    /* Resets the timeout's starting time to the current time */
+    void reset();
+
+    /* Gets whether the timeout has expired or not */
+    bool isExpired();
+
+    /* Stops the timeout so it can never expire */
+    void stop();
+
+    /* Gets whether the timeout was stopped or not */
+    inline bool isStopped()
+    {
+        return _isStopped;
+    }
+private:
+    uint64_t _startTime;
+    bool     _isStopped;
+
+    /* Queries the current time from the operating system */
+    static uint64_t getCurrentTime();
+};
+
+#endif // TIMEOUT_hpp
