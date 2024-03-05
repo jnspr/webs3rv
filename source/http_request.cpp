fa5a143051428b31f8352d219ab1375a803e4099
jsprenge <jsprenge@student.42wolfsburg.de>
2024-03-05 18:14:44 +0100

    http_request: Add headers and remove redundancy
    - Remove redundant body length field
    - Remove redundent public specifier

--- source/http_request.cpp
+++ source/http_request.cpp
@@ -1,5 +1,36 @@
 #include "http_request.hpp"
 
+/* Constructs a HTTP header pair using its key and value */
+HttpRequest::Header::Header(const std::string &key, const std::string &value)
+    : _key(key)
+    , _value(value)
+{
+}
+
+/* Case-invariantly checks if the given key matches with the header's key */
+bool HttpRequest::Header::matchKey(Slice key) const
+{
+    if (_key.size() != key.getLength())
+        return false;
+    for (size_t index = 0; index < key.getLength(); index++)
+    {
+        if (std::tolower(_key[index]) != std::tolower(key[index]))
+            return false;
+    }
+    return true;
+}
+
+/* Case-invariantly finds a header in the given vector of headers */
+const HttpRequest::Header *HttpRequest::findHeaderIn(const std::vector<Header> &headers, Slice key)
+{
+    for (size_t index = 0; index < headers.size(); index++)
+    {
+        if (headers[index].matchKey(key))
+            return &headers[index];
+    }
+    return NULL;
+}
+
 /* Constructs a HTTP request parser using the given rules */
 HttpRequest::Parser::Parser(const ServerConfig &config, uint32_t host, uint16_t port)
 {
--- source/http_request.hpp
+++ source/http_request.hpp
@@ -12,6 +12,31 @@
 
 struct HttpRequest
 {
+    class Header
+    {
+    public:
+        /* Constructs a HTTP header pair using its key and value */
+        Header(const std::string &key, const std::string &value);
+
+        /* Case-invariantly checks if the given key matches with the header's key */
+        bool matchKey(Slice key) const;
+
+        /* Gets the header's key */
+        inline const std::string &getKey()
+        {
+            return _key;
+        }
+
+        /* Gets the header's value */
+        inline const std::string &getValue()
+        {
+            return _value;
+        }
+    private:
+        std::string _key;
+        std::string _value;
+    };
+
     class Parser
     {
     public:
@@ -30,21 +55,29 @@ struct HttpRequest
     Slice                queryPath;       // Query path; eg. "/cgi-bin/demo.py"
     Slice                queryParameters; // Query parameters; eg. "hello=world&abc=def"
     bool                 isLegacy;        // True when HTTP/1.0 instead of HTTP/1.1
+    std::vector<Header>  headers;
     std::vector<uint8_t> body;
-    size_t               bodylength;
 
-    public:
-            /* Gets URL-decoded path of the request */
-        inline std::string getPath() const
-        {
-            return queryPath.toString();
-        }
+    /* Gets URL-decoded path of the request */
+    inline std::string getPath() const
+    {
+        return queryPath.toString();
+    }
 
-            /* Sets the queryPath to newpath*/
-        inline void setPath(std::string newpath)
-        {
-            queryPath = newpath;
-        }
+    /* Sets the queryPath to newpath*/
+    inline void setPath(std::string newpath)
+    {
+        queryPath = newpath;
+    }
+
+    /* Case-invariantly finds a header in the current request */
+    inline const Header *findHeader(Slice key)
+    {
+        return findHeaderIn(headers, key);
+    }
+
+    /* Case-invariantly finds a header in the given vector of headers */
+    static const Header *findHeaderIn(const std::vector<Header> &headers, Slice key);
 };
 
 #endif // HTTP_REQUEST_hpp
