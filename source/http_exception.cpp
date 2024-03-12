189c32208b14bfb0e4a008bd29c7f33e205aa62b
jsprenge <jsprenge@student.42wolfsburg.de>
2024-03-12 18:43:56 +0100

    http: Add constructor to HttpException

--- source/http_exception.cpp
+++ source/http_exception.cpp
@@ -1,5 +1,11 @@
 #include "http_exception.hpp"
 
+/* Constructs a parser exception using the given status code */
+HttpException::HttpException(int statusCode)
+    : _statusCode(statusCode)
+{
+}
+
 /* Gets the cause of the error as a C-style string */
 const char *HttpException::what() const throw()
 {
