2003d34f8ded1fb94aa289bdeeb75b8b07d86f0c
fvon-nag <fvon-nag@student.42wolfsburg.de>
2024-03-22 11:31:32 +0100

    main.cpp : removed unused debugging function

--- source/main.cpp
+++ source/main.cpp
@@ -6,30 +6,6 @@
 
 #include <iostream>
 
-void parseupload(std::vector<uint8_t> body, ssize_t contentLength, uploadData &data)
-{
-    // body.data is used for getting char * from vector maybe need to cast this to char *
-
-    // need to add -- to beginning of boundary for start and -- to the end to end
-    // all values from header are in request vector headers (not finished yet)
-    (void)contentLength;
-    // gets the filesize from the http header via strnstr
-    printf("Parsing upload\n");
-    printf("Contentlength: %ld\n", contentLength);
-
-    // reads the upload body (file) into a new buffer
-
-
-    // creates a slice from the bodybuffer
-     Slice sliceBod((char *) body.data(), body.size());
-   // sliceBuf.splitStart('\n', data.boundary);
-
-
-   // std::cout << "Boundary: \n" << data.boundary << std::endl;
-    std::cout << "slicebuf after first slice: " << sliceBod << std::endl;
-   (void) data;
-}
-
 int main(int argc, char *argv[])
 {
     // If the configuration path was not specified, display usage and quit
